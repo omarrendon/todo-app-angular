21fbd0c0e0ab366236d5abc76b42c43041d32170
Omar Rendon <omarcr.96@gmail.com>
2023-05-26 13:27:32 -0600

    Create new components to list and form in dbz module

---
+++ src/app/dbz/components/add-task/add-task.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.scss'],
+})
+export class AddTaskComponent {}
--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, ListComponent, AddTaskComponent],
   exports: [MainPageComponent],
   imports: [CommonModule],
 })
